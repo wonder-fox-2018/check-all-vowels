6b751251fac16eb60739a8272a87782be05440d5
fmurtadho <fmurtadho@gmail.com>
2018-09-04 20:54:39 +0700

    check all vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,23 @@
+function vowels (str){
+    var dictionary = 'aiueo'
+    var check = []
+    for ( var i = 0 ; i < str.length ; i ++){
+        let current = str[i]
+        for ( var j = 0 ; j < dictionary.length ; j ++){
+            if(current === dictionary[j]){
+                check.push(current)
+            }
+        }
+    }
+    
+    if(str.length === check.length){
+        return true
+    }else{
+        return false
+    }
+}
 
+console.log(vowels('aiueo')) //true
+console.log(vowels('sapisuper')) //false
+console.log(vowels('aaaaaaiiiiiiuuuuuueeeooo')) //true
+console.log(vowels('aiueoaiueoxxx')) //false
