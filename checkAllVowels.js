481b1be00954e29a11a45aceb75568a2cb5ce44a
reshalem <aliisetia@gmail.com>
2018-09-04 21:15:14 +0700

    Tugas Check All Vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,14 @@
+function checkAllVowels(str) {
+	for (var i = 0; i < str.length; i++) {
+		if (str[i] !== 'a' && str[i] !== 'i' && str[i] !== 'u' && str[i] !== 'e' && str[i] !== 'o') {
+			return true;
+		}
+	}
 
+	return false;
+}
+
+console.log(checkAllVowels('aiueox')); // TRUE
+console.log(checkAllVowels('aiueo')); // FALSE
+console.log(checkAllVowels('aiyueo')); // TRUE
+console.log(checkAllVowels('aiu')); // FALSE
