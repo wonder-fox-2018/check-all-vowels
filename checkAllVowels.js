f42c4970cddc94c0a4a875a1bbca9ff1bdc25944
arul21 <arul.21@hotmail.com>
2018-09-04 14:29:11 +0700

    cekAllVowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
-
+function vocal (str){
+    var voc = ['a','i','u','e','o','A','I','U','E','O']
+    var count = 0
+    for(var i = 0; i < str.length; i++){
+        for(var j = 0; j < voc.length; j++){
+            if(str[i] === voc[j]){
+                count++
+            }
+        }
+    }
+    if(count === str.length){
+        return true
+    } else {
+        return false
+    }
+}
+console.log(vocal('aiueoea')); //true
+console.log(vocal('arul')); // false
+console.log(vocal('ARGHSiAXLOiART')); // false
+console.log(vocal('AAAAiIIiiiAAaaaaOOOeeeoooEEEuuuUUUU')); //true
