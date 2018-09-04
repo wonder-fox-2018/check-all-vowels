bda88f33bea628cfa52b8296ba7f08f21bf3ddfa
ajinfadhila <ajinfadhila@gmail.com>
2018-09-04 20:56:12 +0700

    up check vocal

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,23 @@
+function checkvowels(huruf){
 
+  var hrfvocal = ['a','i','u','e','o'];
+  var tmp = ""
+  for (var i = 0; i < huruf.length; i++) {
+    for (var j = 0; j < hrfvocal.length; j++) {
+      // console.log(hrfvocal[j]+" "+ huruf[i])
+      if (hrfvocal[j] === huruf[i]) {
+        tmp += hrfvocal[j]
+      }
+    }
+  }
+  if (tmp.length === huruf.length) {
+    return true
+  }
+  return false
+}
+
+console.log(checkvowels('test'));//FALSE
+console.log(checkvowels('aiuaaaeoa'));//TRUE
+console.log(checkvowels('ajin')); //FALSE
+console.log(checkvowels('berat coy')); //FALSE
+console.log(checkvowels('aiueo')); //TRUE
