fae4f460810459fe99bc5da1ed6a67b303755f86
fathulqorib <adie.share@gmail.com>
2018-09-04 13:00:11 +0700

    ulang , salah baca soal

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,30 +1,10 @@
-function checkVowels(string){
-    var arr = string.split('')
-    var vowelChars = 'aiueoAIUEO'
-    var result = false
-
-    arr.forEach(char => {
-        for (let i = 0; i < vowelChars.length; i++) {
-            const vowelChar = vowelChars[i];
-            if(char == vowelChar) result = true
-        }
-    })
-
-    return result
-}
-
-console.log(checkVowels('adavokal'))
-console.log(checkVowels('gdvkl'))
-
-
-//--- try 2
 function cekvokal(string){
-    var hurufvokal = 'aiueoAIUEO'
+    let hurufvokal = 'aiueoAIUEO'
     for (let i = 0; i < string.length; i++)     
-        if(hurufvokal.indexOf(string[i]) != -1) 
-            return true
+        if(hurufvokal.indexOf(string[i]) == -1) 
+            return false
     
-    return false
+    return true
 }
-console.log(cekvokal('mkaml'))
-console.log(checkVowels('gdvkl'))
+console.log(cekvokal('mkamlaiueo'))
+console.log(cekvokal('aiuuoe'))
