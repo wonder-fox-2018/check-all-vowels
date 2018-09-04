6e57aa00bcf7f4705bf6306fdcdadeeaa4967c0f
pputra <mynameisyoga@live.com>
2018-09-04 11:34:36 +0700

    add solution

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,18 @@
+function checkVowel(str) {
+    var consonants = 'bcdfghjklmnpqrstvwxyz';
 
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < consonants.length; j++) {
+            if (str[i].toLowerCase() === consonants[j]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+console.log(checkVowel('aiueeeoooiu')); //true
+console.log(checkVowel('dauieciioooo')); //false
+console.log(checkVowel('false')); //false
+console.log(checkVowel('aiuCeo')); //false
