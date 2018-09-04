dbe0adb6b957d174a0141b4c79f57d6e9bba5210
Hedya Febritanti <hedya.feb@gmail.com>
2018-09-04 19:54:37 +0700

    checkAllVowels hedyafeb

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,17 @@
+function checkAlVowels(str) {
+    var vowels = 'aiueo';
+    var result = ''
 
+    for (var i=0; i<str.length; i++) {
+        for (var j=0; j<vowels.length; j++) {
+            if (vowels[j] === str[i]) {
+                result += str[i];
+            }
+        } 
+    }
+    return result === str
+}
+
+console.log(checkAlVowels('aioeiiii')); // true
+console.log(checkAlVowels('dasjebcfs')); // false
+console.log(checkAlVowels('ieuiouee')); // true
