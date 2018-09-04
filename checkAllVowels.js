f7c76a14a64530c6b5b539bc89b94be8766a05a6
zenfinish <zen.finish@gmail.com>
2018-09-04 22:51:41 +0700

    upload

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,22 @@
+function checkVowel(data) {
+    if(data.length === 0) {
+        return true;
+    }
+    
+    var kamus = 'aiueoAIUEO';
 
+    var tmp = false;
+    for(var i = 0; i < kamus.length; i++) {
+        if(data[0] === kamus[i]) {
+            tmp = true;
+        }
+    }
+    if(tmp === false) {
+        return false
+    }
+    return checkVowel(data.substr(0, data.length - 1));
+}
+
+console.log(checkVowel('aiueo')); // true
+console.log(checkVowel('zainal')); // false
+console.log(checkVowel('kttzc')); // false
