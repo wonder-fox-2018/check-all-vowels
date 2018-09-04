9499621ca90d9d272db60bd0afa158d9863187d4
Dwi Wicaksono <dwiwicaksono@Ndiw-MacBook-Pro.local>
2018-09-04 10:50:18 +0700

    done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,17 @@
+function checkVowel(word){
+    if (!word) return `Masukan minimal 1 huruf`
+    let vocal = ['a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O' ]
+    for (let i = 0; i < word.length; i++){
+        let cek = false
+        for(let j = 0; j < vocal.length; j++){
+            if (word[i] === vocal[j]){
+                cek = true
+            }
+        }
+        if (cek === false) return false
+    }
+    return true
+}
 
+console.log(checkVowel('ajgjafdj'));
+console.log(checkVowel('aeioeae'));
