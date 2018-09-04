c49f8c787ed036f70025123ed6007d6a86b2ff43
Hilarion18 <hilarion_cross@hotmail.com>
2018-09-04 15:14:33 +0700

    check 1

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
+function checkVowel(kata) {
+    let vowel = 'aiueo';
+    for ( let i = 0; i < vowel.length; i++) {
+        for (let j = 0; j < kata.length; j++) {
+            if ( kata[j] === vowel[i]) {
+                return true
+            }
+        }
+        return false
+    }
+}
 
+console.log(checkVowel('anoa'))      // TRUE
+console.log(checkVowel('anggur'))    // TRUE
+console.log(checkVowel('zebra'))     // TRUE
+console.log(checkVowel('strywgyr'))  // FALSE
