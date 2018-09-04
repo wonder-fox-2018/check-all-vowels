2f3b0e01ba3b131b35593f930cdac828e6835cf4
Your Name <hokandre@mhs.mdp.ac.id>
2018-09-04 14:32:05 +0700

    vowel

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
-
+function cekVowel(kalimat){
+    var kamus='aiueo'
+    for(var i=0;i<kalimat.length;i++){
+        vowel=false
+        for(var j=0;j<kamus.length;j++){
+            if(kalimat[i]===kamus[j]){
+                vowel=true;
+            }
+        }
+        if(vowel===false){
+            return false
+        }
+    }
+    return true
+}
+console.log(cekVowel('aiueo'))
